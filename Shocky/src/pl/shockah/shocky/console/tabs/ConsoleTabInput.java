f534b70c5656409b6d41f6608d2844b9a1a39d21
shockah <lolshockah@gmail.com>
2013-06-03 06:12:13 +0200

    Working on input

--- Shocky/src/pl/shockah/shocky/console/tabs/ConsoleTabInput.java
+++ Shocky/src/pl/shockah/shocky/console/tabs/ConsoleTabInput.java
@@ -2,20 +2,28 @@ package pl.shockah.shocky.console.tabs;
 
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
 import pl.shockah.shocky.console.ConsoleThread;
+import pl.shockah.shocky.console.TextInputThread;
 import com.googlecode.lanterna.terminal.TerminalSize;
 
-public class ConsoleTabOutput extends ConsoleTab {
-	public List<String> lines = new LinkedList<String>();
-	public StringBuffer sb = new StringBuffer();
+public abstract class ConsoleTabInput extends ConsoleTabOutput {
+	protected boolean open = false;
+	protected TextInputThread tit;
 	
-	public ConsoleTabOutput(ConsoleThread ct, String title) {
+	public ConsoleTabInput(ConsoleThread ct, String title) {
 		super(ct,title);
 	}
+	
+	public abstract void onOpen(); //create and start TextInputThread, wait for result
 
 	public void update() {
-		int yy = ct.screen.getTerminalSize().getRows()-1;
+		if (!open) {
+			onOpen();
+			open = true;
+		}
+		
+		TerminalSize size = ct.screen.getTerminalSize();
+		int yy = size.getRows()-3;
 		if (sb.length() > 0) ct.writer.drawString(0,yy--,sb.toString());
 		
 		Iterator<String> it = ((LinkedList<String>)lines).descendingIterator();
@@ -24,7 +32,7 @@ public class ConsoleTabOutput extends ConsoleTab {
 			if (yy < 0) break;
 		}
 		
-		TerminalSize size = ct.screen.getTerminalSize();
+		for (int i = 0; i < size.getColumns(); i++) ct.writer.drawString(i,size.getRows()-2,"=");
 		ct.screen.setCursorPosition(size.getColumns()-1,size.getRows()-1);
 	}
 }
